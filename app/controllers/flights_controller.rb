762f3a6dba7a3f043421f2307a42c4f18dd3e5d9
Brian Boylen <bbbrianboylen@gmail.com>
2020-07-18 21:16:10 -0500

    ughghghgh

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,10 +2,8 @@ class FlightsController < ApplicationController
 
   def index
     @airports = Airport.all
-    @flight = Flight.new
     @from_airport_options = Airport.all.map{ |a| [a.code, a.id]}
-    @to_airport_options = Airport.all.map{ |a| [a.code, a.id]}
-    
+    @to_airport_options = @from_airport_options
   end
 
   def new
